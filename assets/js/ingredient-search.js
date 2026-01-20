c9b1c30998190c2449e9dc24b474b591fa02654c
qmwc3 <qmwc3@aol.com>
2026-01-20 16:52:53 -0600

    Update ingredient-search.js

--- assets/js/ingredient-search.js
+++ assets/js/ingredient-search.js
@@ -14,7 +14,13 @@ document.addEventListener('DOMContentLoaded', function () {
       if (!r.ok) throw new Error('Network response was not ok');
       return r.json();
     })
-    .then(data => { recipes = data; })
+    .then(data => { 
+      // Normalize &nbsp; in ingredients immediately
+      recipes = data.map(r => ({
+        ...r,
+        ingredients: (r.ingredients || []).map(i => i.replace(/&nbsp;/g, ' '))
+      }));
+    })
     .catch((err) => {
       console.error('Failed to load recipes.json:', err);
       if (results) results.innerHTML = '<p class="muted">Could not load recipe index.</p>';
